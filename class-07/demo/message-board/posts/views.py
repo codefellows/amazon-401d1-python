9cb70810c10724c678a55cd487e3e39d1a183069
JB-Tellez <jb.tellez@gmail.com>
2020-05-22 10:52:30 -0700

    adds step 2 of class demo

--- class-07/demo/message-board/posts/views.py
+++ class-07/demo/message-board/posts/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+
+    model = Post
 
     template_name = "home.html"
 
+    context_object_name = "all_posts"
